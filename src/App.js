f4709f11170849e59f18cf3c287d15884c10ca0b
Nicolas Eduardo Ramirez <nramirez@mcti.local>
2019-08-06 13:07:55 -0300

    first try routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { LandingPage } from './landing.page';
+import {Route, Switch } from 'react-router-dom';
+import {AppLayout} from './app.layout';
+import {ProtectedRoute} from './protected.route';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>holanda protected</h1>
+     <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <ProtectedRoute exact path="/app" component={AppLayout} />
+        <Route path="*" component={() => "404 NOT FOUND"}/>
+      </Switch>
     </div>
   );
 }
